7261f173b3eb9e13486f2598ec7aac6b0fe709f8
Josué de Oliveira Mendonça <kanji.flimorago@gmail.com>
2024-09-27 17:24:22 -0300

    *Playing Star Wars Theme*

--- farmarcia/src/Components/pagina-Body/Home.jsx
+++ farmarcia/src/Components/pagina-Body/Home.jsx
@@ -5,24 +5,50 @@ function Home() {
     <>
         <div className='vendas'>
           <div className='produtos'>
-            <img src="/public/img/torsilax-30.webp" height={250} />
+            <center><img src="/public/img/torsilax-30.webp" height={180} /></center>
             <h1>Torsilax Neo química</h1>
-            <p>R$15,30</p>
+            <h2>R$15,30</h2>
           </div>
           <div className='produtos'>
-            <img src="/public/img/ibuprofeno.png" alt="" height={250} />
+            <center><img src="/public/img/ibuprofeno.png" height={180} /></center>
             <h1>Ibuprofeno</h1>
-            <p>R$15,29</p>
+            <div>
+              <h2>R$13,76</h2>
+              <h3><s>R$15,29</s></h3>
+              <h1>10% OFF</h1>
+            </div>
           </div>
           <div className='produtos'>
-            <img src="/public/img/dorflex.webp" alt="" height={250} />
+            <center>
+              <img src="/public/img/dorflex.webp" height={180} />
+            </center>
             <h1>Dorflex</h1>
-            <p>R$20,00</p>
+            <div>
+              <h2>R$10,80</h2>
+              <h3><s>R$12,00</s></h3>
+              <p></p>
+              <h1>10% OFF</h1>
+            </div>
           </div>
           <div className='produtos'>
-            <img src="/public/img/dipirona.webp" alt="" height={250} />
+            <center>
+              <img src="/public/img/dipirona.webp" height={180} />
+            </center>
             <h1>Dipirona</h1>
-            <p>R$13,00</p>
+            <div>
+              <h2>R$13,00</h2>
+              <h1>10% OFF</h1>
+            </div>
+          </div>
+          <div className='produtos'>
+            <center>
+              <img src="/public/img/whey-protein.png" height={180} />
+            </center>
+            <h1>Whey Protein</h1>
+            <div>
+            <h2>R$35,00</h2>
+            <h1>10% OFF</h1>
+            </div>
           </div>
         </div>
     </>
--- farmarcia/src/components/Header.jsx
+++ farmarcia/src/components/Header.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 function Header() {
   return (
     <nav className='header'>
-      <img src="/img/farmarcia.PNG.png" id='logo-nav' />
+      <img src="/img/farmarcia.PNG.png" height={120} />
       <button id='sobre-nos'>Sobre nós</button>
       <button>Ofertas</button>
       <button>Cadastro / Login</button>
+      <form action=''>
+      <input type="text" placeholder='Pesquisar produto...' /><input type="submit" />
+      </form>
     </nav>
   )
 }
